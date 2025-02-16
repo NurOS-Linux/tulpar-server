e8c86a9dcd871472688e95f99960700fbc01b951
Meigo <73817505+meigoc@users.noreply.github.com>
2025-02-16 12:58:55 +0300

    beta 1

---
+++ org/meigo/t/classes/Main.java
@@ -0,0 +1,15 @@
+package org.meigo.t.classes;
+
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.handler.DefaultHandler;
+import org.eclipse.jetty.server.handler.HandlerCollection;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+
+
+    }
+}
---
+++ org/meigo/t/classes/StaticFileServlet.java
@@ -0,0 +1,41 @@
+package org.meigo.t.classes;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.nio.file.Files;
+
+public class StaticFileServlet extends HttpServlet {
+    private static final String ICONS_DIR = "icons"; // Папка для иконки
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String path = req.getRequestURI().substring("/icons/".length()); // Убираем часть /icons/
+        File file = new File(ICONS_DIR, path);
+
+        if (!file.exists()) {
+            resp.sendError(HttpServletResponse.SC_NOT_FOUND, "Icon not found.");
+            return;
+        }
+
+        // Определяем MIME-тип файла
+        String contentType = Files.probeContentType(file.toPath());
+        resp.setContentType(contentType != null ? contentType : "application/octet-stream");
+
+        // Отправляем файл
+        try (FileInputStream fis = new FileInputStream(file);
+             OutputStream os = resp.getOutputStream()) {
+            byte[] buffer = new byte[1024];
+            int bytesRead;
+            while ((bytesRead = fis.read(buffer)) != -1) {
+                os.write(buffer, 0, bytesRead);
+            }
+        }
+    }
+}
+
